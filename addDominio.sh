ed4ae108f5a5f6ef59dcab44e47cd9ee6dd5aba4
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2017-12-26 18:58:33 +0100

    Se añade valor por defecto

--- addDominio.sh
+++ addDominio.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 
-read -p "Nombre de Dominio: (Entre comillas): " dominio
+read -p "Nombre de Dominio (Entre comillas): " dominio
 
+if [ -z $dominio ]
+then
+    echo "Se establece dominio.local"
+    dominio=dominio.local
+fi
 
 echo "127.0.0.1	$dominio" >> /etc/hosts
 
