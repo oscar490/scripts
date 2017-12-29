ebb9a5259077bbdfcd1d83631cccb34a61b25f97
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2017-12-29 16:52:04 +0100

    Se ha creado addDominio.sh

--- inicio.sh
+++ inicio.sh
@@ -1,1 +1,13 @@
-sudo ./addDominio.sh
+
+sudo ./addDominio.sh $1
+
+if [ $? -eq 0 ]
+then
+    echo Se ha añadido el nombre de host correctamente.
+
+else
+    echo No se ha añadido el nombre del host correctamente
+    exit
+fi
+
+echo continuacion
