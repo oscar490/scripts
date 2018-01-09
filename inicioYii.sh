4ddff1bdf2e5423af004a15db9d2a60deb7343d8
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2018-01-09 12:20:42 +0100

    Mas organizacon

--- inicioYii.sh
+++ inicioYii.sh
@@ -12,11 +12,11 @@ mostrar() {
     fi
 }
 
-sudo ./addDominio.sh $1
+sudo ./scripts/addDominio.sh $1
 mostrar "se ha añadido el nombre de host correctamente"
 
-sudo ./addVirtualHost.sh $1 "/var/www/web/$1/web"
+sudo ./scripts/addVirtualHost.sh $1 "/var/www/web/$1/web"
 mostrar "se ha añadido el sitio vitual correctamente"
 
-./addProyectoYiiBasico.sh $1
+./scripts/addProyectoYiiBasico.sh $1
 #mostrar "se ha añadido la estructura de carpetas"  ~/web/$1
